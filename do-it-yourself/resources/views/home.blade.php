b8ca116056d60d79de0c69b15836b339fa1b7f86
Jeth0214 <rolandjethrosuyom@yahoo.com>
2021-05-22 07:59:33 +0300

    implemented user's ideas pagination

--- do-it-yourself/resources/views/home.blade.php
+++ do-it-yourself/resources/views/home.blade.php
@@ -3,6 +3,35 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
+
+        @foreach ($ideas as $idea)
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ $idea->user_id }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+                </div>
+            </div>
+        </div>
+        @endforeach
+
+    </div>
+</div>
+@endsection
+
+
+
+
+
+
+<!-- <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
@@ -18,6 +47,4 @@
                 </div>
             </div>
         </div>
-    </div>
-</div>
-@endsection
+    </div> -->
