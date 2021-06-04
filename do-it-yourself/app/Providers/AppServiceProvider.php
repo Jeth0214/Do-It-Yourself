230d7cf3c7a763f143a514010abd05d421b3c5a9
Jeth0214 <rolandjethrosuyom@yahoo.com>
2021-06-04 18:56:39 +0300

    update appserviceprovider to fix js and css not working

--- do-it-yourself/app/Providers/AppServiceProvider.php
+++ do-it-yourself/app/Providers/AppServiceProvider.php
@@ -27,5 +27,8 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Paginator::useBootstrap();
+        if ($this->app->environment('production')) {
+            \URL::forceScheme('https');
+        };
     }
 }
